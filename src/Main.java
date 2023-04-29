50d38dfd98851cf36d20499c859ad11fd23aa381
Angelina <a.hurok@gmail.com>
2023-04-30 01:08:47 +0300

    added chess

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,18 @@ import static java.lang.Math.sqrt;
 (x – a)^2 + (y – b)^2 ≤ r^2 */
 
 public class Main {
-    public static boolean schastlivoe_pislo (int x)
+
+    public static boolean ferZ(int x1, int y1, int x2, int y2){
+        if((x1 == x2) || (y1 == y2) || ((x1 + y1 == x2 + y2) || (x1 - y1 == x2 - y2)))
+            return true;
+        else return false;
+    }
+    /*public static boolean schastlivoe_pislo (int x)
     {
         if ((x / 1000 + (x / 100) % 10) == ((x / 10) % 10 + x % 10) )
         return true;
         else return false;
-    }
+    }*/
     /*public static boolean checkPoint (int x, int y, int r, int a, int b){
         //(x – a)2 + (y – b)2 ≤ r2
         if ((sqrt(x-a) + sqrt(y-b)) <= (sqrt(r)))
@@ -41,10 +47,23 @@ public class Main {
          * Определить, счастливое ли заданное число, вернуть true, если это так.
          */
 
-        int pislo;
-        pislo = in.nextInt();
+        /**
+         * Простая (2 балла)
+         *
+         * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
+         * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
+         * Считать, что ферзи не могут загораживать друг друга.
+         */
+        /*int pislo;
+        pislo = in.nextInt();*/
 
-        System.out.println(schastlivoe_pislo(pislo));
+        int x1, y1, x2, y2;
+
+        x1 = in.nextInt();
+        y1 = in.nextInt();
+        x2 = in.nextInt();
+        y2 = in.nextInt();
+        System.out.println(ferZ(x1, y1, x2, y2));
     }
 
 }
