6e2a289da87780f12c962e64306e76ae06ca2072
Angelina <a.hurok@gmail.com>
2023-04-30 01:46:17 +0300

    added caledar

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,11 @@ import static java.lang.Math.sqrt;
 
 public class Main {
 
-    public static boolean ferZ(int x1, int y1, int x2, int y2){
+    /* public static boolean ferZ(int x1, int y1, int x2, int y2){
         if((x1 == x2) || (y1 == y2) || ((x1 + y1 == x2 + y2) || (x1 - y1 == x2 - y2)))
             return true;
         else return false;
-    }
+    }*/
     /*public static boolean schastlivoe_pislo (int x)
     {
         if ((x / 1000 + (x / 100) % 10) == ((x / 10) % 10 + x % 10) )
@@ -30,6 +30,19 @@ public class Main {
         else
             return false;
     }*/
+    public static int colendar (int mes, int god)
+    {
+        if ((mes != 2) && ((mes <= 7) && (mes % 2 == 1)) || ((mes > 7) && (mes % 2 ==0)))
+            return 31;
+        else
+            if ((mes != 2) && ((mes <= 7) && (mes % 2 == 0)) || ((mes > 7) && (mes % 2 ==1)))
+                return 30;
+            else
+                if (god % 4 == 0)
+                    return 29;
+                else return 28;
+
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
       /*  int x, y, r, a, b;
@@ -57,13 +70,26 @@ public class Main {
         /*int pislo;
         pislo = in.nextInt();*/
 
-        int x1, y1, x2, y2;
+        /*int x1, y1, x2, y2;
 
         x1 = in.nextInt();
         y1 = in.nextInt();
         x2 = in.nextInt();
-        y2 = in.nextInt();
-        System.out.println(ferZ(x1, y1, x2, y2));
+        y2 = in.nextInt();*/
+        /**
+         * Простая (2 балла)
+         *
+         * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
+         * Вернуть число дней в этом месяце этого года по григорианскому календарю.
+         */
+        int mes, god;
+        do{
+            System.out.println("Input month (shouLd be more than 0 and less than 13): ");
+            mes = in.nextInt();
+            System.out.println("Input year (shouLd be more than 0): ");
+            god = in.nextInt();
+        }while((mes > 12) || (god <= 0) || (mes < 1));
+        System.out.println(colendar(mes, god));
     }
 
 }
