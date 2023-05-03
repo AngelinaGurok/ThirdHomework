765935e37cadcfedd41b72f7300ecde012fad42f
Angelina <a.hurok@gmail.com>
2023-05-03 15:44:50 +0300

    added triangleType function

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,49 @@ import static java.lang.Math.sqrt;
 (x – a)^2 + (y – b)^2 ≤ r^2 */
 
 public class Main {
-    public static boolean brick(int a, int b, int c, int r, int s){
+    public static int triangleType(int a, int b, int c){
+        if (a > b + c || b > a + c || c > a + b) return -1;
+        if(a >= b && a >= c){
+            if(a * a < b * b + c * c){
+                return 0;
+            }
+            if(a * a == b * b + c * c){
+                return 1;
+            }
+            if(a * a > b * b + c * c){
+                return 2;
+            }
+        } else if (b >= a && b >= c){
+            if(b * b < a * a + c * c){
+                return 0;
+            }
+            if(b * b == a * a + c * c){
+                return 1;
+            }
+            if(b * b > a * a + c * c){
+                return 2;
+            }
+
+        } else {
+            if(c * c < a * a + b * b){
+                return 0;
+            }
+            if(c * c == a * a + b * b){
+                return 1;
+            }
+            if(c * c > a * a + b * b){
+                return 2;
+            }
+        }
+        return 5;
+    }
+    /*public static boolean brick(int a, int b, int c, int r, int s){
         if((a <= r && (b <= s || c <= s)) || (b <= r && (a <= s || c <= s)) ||
                 (c <= r && (b <= s || a <= s))){
             return true;
         }
         else return false;
-    }
+    }*/
 
     /**(a1 - a2)^2 + (b1-b2)^2 < (r-r2)^2*/
     /*public static boolean circles(double x1, double y1, double r1, double x2, double y2, double r2){
@@ -64,13 +100,20 @@ public class Main {
     }*/
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        /**
+         * Пример
+         *
+         * Лежит ли точка (x, y) внутри окружности с центром в (x0, y0) и радиусом r?
+         */
+
         /*  int x, y, r, a, b;
         x = in.nextInt();
         y = in.nextInt();
         r = in.nextInt();
         a = in.nextInt();
         b = in.nextInt();
-        boolean out = checkPoint(x, y, r, a, b);*/
+        System.out.println(chessQueens(checkPoint(x, y, r, a, b)); */
 
         /**
          * Простая (2 балла)
@@ -79,6 +122,10 @@ public class Main {
          * Определить, счастливое ли заданное число, вернуть true, если это так.
          */
 
+        /*int num;
+        num = in.nextInt();
+        System.out.println(chessQueens(luckyNum(num));*/
+
         /**
          * Простая (2 балла)
          *
@@ -86,8 +133,7 @@ public class Main {
          * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
          * Считать, что ферзи не могут загораживать друг друга.
          */
-        /*int num;
-        num = in.nextInt();*/
+
 
         /*int x1, y1, x2, y2;
 
@@ -108,10 +154,8 @@ public class Main {
             month = in.nextInt();
             System.out.println("Input year (shouLd be more than 0): ");
             year = in.nextInt();
-        }while((mes > 12) || (god <= 0) || (mes < 1));*/
-
-
-
+        }while((mes > 12) || (god <= 0) || (mes < 1));
+        System.out.println(calendar(month, year));*/
 
         /**
          * Простая (2 балла)
@@ -120,6 +164,7 @@ public class Main {
          * окружности с центром в (x2, y2) и радиусом r2.
          * Вернуть true, если утверждение верно
          */
+
         /*double x1, y1, r1, x2, y2, r2;
         x1 = in.nextDouble();
         y1 = in.nextDouble();
@@ -138,14 +183,26 @@ public class Main {
          * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
          * Вернуть true, если кирпич пройдёт
          */
-         int a, b, c, r, s;
+        /*int a, b, c, r, s;
          a = in.nextInt();
          b = in.nextInt();
          c = in.nextInt();
          r = in.nextInt();
          s = in.nextInt();
-         System.out.println(brick(a, b, c, r, s));
-
+         System.out.println(brick(a, b, c, r, s));*/
+        /**
+         * Простая (2 балла)
+         *
+         * Треугольник задан длинами своих сторон a, b, c.
+         * Проверить, является ли данный треугольник остроугольным (вернуть 0),
+         * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
+         * Если такой треугольник не существует, вернуть -1.
+         */
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        System.out.println(triangleType(a, b, c));
     }
 
 }
