1d2f07fadbbbdad43308cd3116563167c25d2a19
Angelina <a.hurok@gmail.com>
2023-05-03 15:10:36 +0300

    renamed variables, added brick function

--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,44 @@ import static java.lang.Math.sqrt;
 (x – a)^2 + (y – b)^2 ≤ r^2 */
 
 public class Main {
-    public static boolean circles(double x1, double y1, double r1, double x2, double y2, double r2){
-        //(a1 - a2)^2 + (b1-b2)^2 < (r-r2)^2
-        if((pow((x1-x2), 2) + pow((x1-x2), 2)) < pow((r1-r2), 2))
+    public static boolean brick(int a, int b, int c, int r, int s){
+        if((a <= r && (b <= s || c <= s)) || (b <= r && (a <= s || c <= s)) ||
+                (c <= r && (b <= s || a <= s))){
             return true;
+        }
         else return false;
     }
-    /* public static boolean ferZ(int x1, int y1, int x2, int y2){
+
+    /**(a1 - a2)^2 + (b1-b2)^2 < (r-r2)^2*/
+    /*public static boolean circles(double x1, double y1, double r1, double x2, double y2, double r2){
+        if((pow((x1 - x2), 2) + pow((x1 - x2), 2)) < pow((r1 - r2), 2))
+            return true;
+        else return false;
+    }*/
+
+    /* public static boolean chessQueens(int x1, int y1, int x2, int y2){
         if((x1 == x2) || (y1 == y2) || ((x1 + y1 == x2 + y2) || (x1 - y1 == x2 - y2)))
             return true;
         else return false;
     }*/
-    /*public static boolean schastlivoe_pislo (int x)
+
+    /*public static boolean luckyNum (int x)
     {
         if ((x / 1000 + (x / 100) % 10) == ((x / 10) % 10 + x % 10) )
         return true;
         else return false;
     }*/
+
+    /** (x – a)2 + (y – b)2 ≤ r2 */
     /*public static boolean checkPoint (int x, int y, int r, int a, int b){
-        //(x – a)2 + (y – b)2 ≤ r2
-        if ((sqrt(x-a) + sqrt(y-b)) <= (sqrt(r)))
-         return true;
+
+        if ((pow((x - a), 2) + pow((y - b), 2) <= (pow(r, 2)))
+            return true;
         else
             return false;
     }*/
-    /* public static int colendar (int mes, int god)
+
+    /* public static int calendar (int mes, int god)
     {
         if ((mes != 2) && ((mes <= 7) && (mes % 2 == 1)) || ((mes > 7) && (mes % 2 ==0)))
             return 31;
@@ -73,28 +86,33 @@ public class Main {
          * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
          * Считать, что ферзи не могут загораживать друг друга.
          */
-        /*int pislo;
-        pislo = in.nextInt();*/
+        /*int num;
+        num = in.nextInt();*/
 
         /*int x1, y1, x2, y2;
 
         x1 = in.nextInt();
         y1 = in.nextInt();
         x2 = in.nextInt();
-        y2 = in.nextInt();*/
+        y2 = in.nextInt();
+        System.out.println(chessQueens(x1, y1, r1, x2, y2));*/
         /**
          * Простая (2 балла)
          *
          * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
          * Вернуть число дней в этом месяце этого года по григорианскому календарю.
          */
-        /*int mes, god;
+        /*int month, year;
         do{
             System.out.println("Input month (shouLd be more than 0 and less than 13): ");
-            mes = in.nextInt();
+            month = in.nextInt();
             System.out.println("Input year (shouLd be more than 0): ");
-            god = in.nextInt();
+            year = in.nextInt();
         }while((mes > 12) || (god <= 0) || (mes < 1));*/
+
+
+
+
         /**
          * Простая (2 балла)
          *
@@ -102,15 +120,32 @@ public class Main {
          * окружности с центром в (x2, y2) и радиусом r2.
          * Вернуть true, если утверждение верно
          */
-        //(a1 - a2)^2 + (b1-b2)^2 < (r-r2)^2
-        double x1, y1, r1, x2, y2, r2;
+        /*double x1, y1, r1, x2, y2, r2;
         x1 = in.nextDouble();
         y1 = in.nextDouble();
         r1 = in.nextDouble();
         x2 = in.nextDouble();
         y2 = in.nextDouble();
         r2 = in.nextDouble();
-        System.out.println(circles(x1, y1, r1, x2, y2, r2));
+        System.out.println(circles(x1, y1, r1, x2, y2, r2));*/
+
+        /**
+         * Средняя (3 балла)
+         *
+         * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
+         * Стороны отверстия должны быть параллельны граням кирпича.
+         * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
+         * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
+         * Вернуть true, если кирпич пройдёт
+         */
+         int a, b, c, r, s;
+         a = in.nextInt();
+         b = in.nextInt();
+         c = in.nextInt();
+         r = in.nextInt();
+         s = in.nextInt();
+         System.out.println(brick(a, b, c, r, s));
+
     }
 
 }
